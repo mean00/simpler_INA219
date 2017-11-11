72b409b3b8e7778554f8e86055f40d33d20600d2
mean <fixounet@free.fr>
2017-11-11 10:13:24 +0100

    [simplerINA219] example + get Resolution

---
+++ examples/getcurrent/simplerIna219.ino
@@ -0,0 +1,98 @@
+//
+//
+// Sample demo of simpler_INA219
+//
+#include <Wire.h>
+#include <simpler_INA219.h>
+
+
+simpler_INA219 *sensor219; // Declare and instance of INA219
+
+#define VERBOSE 1
+/**
+ * 
+ */
+void setup(void) 
+{
+      
+  Serial.begin(57600);    
+  Serial.print("Start");
+  Serial.print("Sensor initialization\n");
+  sensor219=new simpler_INA219(INA219_ADDRESS,20);  // we use 20 mOhm shunt
+  sensor219->setMultiSampling(2);                   // 4 samples
+  sensor219->begin();
+  Serial.println("Setup done\n");  
+#if 0 // if you want to do autozero...
+  Serial.print("Autozeroing\n");  
+  sensor219->autoZero();                            // /!\ Must have zero current here!!!
+  Serial.print("Autozeroing done\n"); 
+#else
+  sensor219->setZeroOffset(0,-1); // or if you know them.... beware they can change a bit over time
+  sensor219->setZeroOffset(1,-1); // the error is then dependant on the shunt, see getResolutionMicroAmp()
+  sensor219->setZeroOffset(2,-1);
+  sensor219->setZeroOffset(3,-1);
+#endif
+}
+/**
+ * 
+ */
+void float2str(char *s,float f,const char *unit)
+{
+  float scale=1;
+  const char *ext=" ";
+  if(f<1.1)
+  {
+    f=f*1000.;
+     sprintf(s,"%04dm%s",(int)f,unit);
+     return;
+  }
+  
+  int zleft=(int)(f);
+  float g=f-(float)(zleft);
+  int right=(int)(g*10.);
+  sprintf(s,"%02d",zleft);
+  s[2]='.';
+  s+=3;
+  sprintf(s,"%01d",right);
+  s+=1;
+  sprintf(s,"%s%s",ext,unit);
+  
+}
+/**
+ * 
+ */
+void loop(void) 
+{
+  float busVoltage = 0;
+  int current = 0; // Measure in milli amps
+  
+
+  char stA[32];
+  char stV[32];
+
+
+  busVoltage = sensor219->getBusVoltage_V();
+  current = sensor219->getCurrent_mA();
+  
+  sprintf(stA,"Scale=%d",sensor219->getCurrentScaler()); // current scale
+  Serial.println(stA);
+
+  sprintf(stA,"uA per bit=%d",sensor219->getResolutionMicroAmp()); // get accuracy with current scale
+  Serial.println(stA);
+  
+  
+    
+  float2str(stA,((float)(current)),"mA");  
+  float2str(stV,busVoltage,"V");  
+  Serial.println(stV);
+  Serial.println("-----------------\n");
+  Serial.println(stA);
+  Serial.println("-----------------\n");
+  
+  int raw=sensor219->getShuntVoltage_raw();
+  sprintf(stA,"RAW = %d\n",(int)raw);
+  Serial.println(stA);
+  delay(500);
+}
+// EOF
+
--- simpler_INA219.cpp
+++ simpler_INA219.cpp
@@ -30,6 +30,41 @@
 #include <Wire.h>
 
 #include "simpler_INA219.h"
+#include "simpler_INA219_internal.h"
+
+/**
+ */
+typedef struct 
+{
+    int   registerValue;
+    float millivoltRange;
+}ina219Scaler;
+/**
+ */
+static const ina219Scaler scaler[4]=
+{
+    {INA219_CONFIG_GAIN_1_40MV,40.},
+    {INA219_CONFIG_GAIN_2_80MV,80.},
+    {INA219_CONFIG_GAIN_4_160MV,160.},
+    {INA219_CONFIG_GAIN_8_320MV,320.},
+};
+
+
+/**************************************************************************/
+/*! 
+    @brief  Instantiates a new INA219 class
+*/
+/**************************************************************************/
+simpler_INA219::simpler_INA219(uint8_t addr,int shunt) 
+{
+  ina219_i2caddr = addr;
+  ina219_shuntValueMillOhm=shunt;  
+  ina219_currentScale=3;
+  for(int i=0;i<4;i++)
+    ina219_zeros[i]=0;
+  multiSampling=0;
+  reconfigure();
+}
 
 /**************************************************************************/
 /*! 
@@ -79,28 +114,35 @@ void simpler_INA219::wireReadRegister(uint8_t reg, uint16_t *value)
   #endif
 }
 
-
-/**************************************************************************/
-/*! 
-    @brief  Instantiates a new INA219 class
-*/
-/**************************************************************************/
-simpler_INA219::simpler_INA219(uint8_t addr,int shunt) 
+/**
+ * 
+ */
+void simpler_INA219::reconfigure(void)
 {
-  ina219_i2caddr = addr;
-  ina219_shuntValueMillOhm=shunt;
-  ina219_zero=0;
+  // Set Config register to take into account the settings above
+  uint16_t config = INA219_CONFIG_BVOLTAGERANGE_32V |
+                    scaler[ina219_currentScale].registerValue |
+                    INA219_CONFIG_BADCRES_12BIT |
+                    INA219_CONFIG_SADCRES_12BIT_1S_532US |
+                    INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
+  wireWriteRegister(INA219_REG_CONFIG, config);
+  
+  multiplier=1<<ina219_currentScale;
+  multiplier=(scaler[ina219_currentScale].millivoltRange/(float)ina219_shuntValueMillOhm)/multiplier;
+  multiplier/=4.096;
+  
 }
+
 
 /**************************************************************************/
 /*! 
     @brief  Setups the HW (defaults to 32V and 2A for calibration values)
 */
 /**************************************************************************/
-void simpler_INA219::begin() {
+void simpler_INA219::begin() 
+{
   Wire.begin();    
-  // Set chip to known config values to start
-
+  reconfigure();
 }
 
 /**************************************************************************/
@@ -108,10 +150,10 @@ void simpler_INA219::begin() {
     @brief  Gets the raw bus voltage (16-bit signed integer, so +-32767)
 */
 /**************************************************************************/
-int16_t simpler_INA219::getBusVoltage_raw() {
+int16_t simpler_INA219::getBusVoltage_raw() 
+{
   uint16_t value;
   wireReadRegister(INA219_REG_BUSVOLTAGE, &value);
-
   // Shift to the right 3 to drop CNVR and OVF and multiply by LSB
   return (int16_t)((value >> 3) * 4);
 }
@@ -121,22 +163,13 @@ int16_t simpler_INA219::getBusVoltage_raw() {
     @brief  Gets the raw shunt voltage (16-bit signed integer, so +-32767)
 */
 /**************************************************************************/
-int16_t simpler_INA219::getShuntVoltage_raw() {
+int simpler_INA219::getShuntVoltage_raw() 
+{
   uint16_t value;
   wireReadRegister(INA219_REG_SHUNTVOLTAGE, &value);
   return (int16_t)value;
 }
 
-/**************************************************************************/
-/*! 
-    @brief  Gets the shunt voltage in mV (so +-327mV)
-*/
-/**************************************************************************/
-float simpler_INA219::getShuntVoltage_mV() {
-  int16_t value;
-  value = getShuntVoltage_raw();
-  return value ;
-}
 
 /**************************************************************************/
 /*! 
@@ -145,7 +178,7 @@ float simpler_INA219::getShuntVoltage_mV() {
 /**************************************************************************/
 float simpler_INA219::getBusVoltage_V() {
   int16_t value = getBusVoltage_raw();
-  return value * 0.001;
+  return value * 0.001; // 10mv / tick
 }
 
 /**************************************************************************/
@@ -154,11 +187,71 @@ float simpler_INA219::getBusVoltage_V() {
             config settings and current LSB
 */
 /**************************************************************************/
-float simpler_INA219::getCurrent_mA() {
-  float valueDec = getShuntVoltage_raw();
-  valueDec-=ina219_zero;
-  valueDec=valueDec*40./(float)ina219_shuntValueMillOhm;
-  valueDec/=4.096;
+int simpler_INA219::getOneCurrent_mA() {
+again:    
+  int raw=getShuntVoltage_raw();  
+  // Check if we are below 1/4 of the amp ...
+  if(raw<=1024 && ina219_currentScale )
+  {
+      ina219_currentScale--;
+      reconfigure();
+      goto again;
+  }
+  // same for upper boundary
+  if(raw>1024*3 && ina219_currentScale<4)
+  {
+      ina219_currentScale++;
+      reconfigure();
+      goto again;
+  }
   
+  raw-=ina219_zeros[ina219_currentScale];
+  float valueDec=raw;
+  valueDec*=multiplier;
   return valueDec;
+}
+
+int simpler_INA219::getCurrent_mA() 
+{
+    if(!multiSampling) return getOneCurrent_mA();
+    float sum=0;
+    int linear=1<<multiSampling;
+    for(int i=0;i<linear;i++)
+    {
+        sum+=getOneCurrent_mA();
+        delay(1);
+    }
+    sum/=(float)linear;
+    return (int)sum;
+}
+
+/**
+ * 
+ * @return 
+ */
+int simpler_INA219::getResolutionMicroAmp()
+{
+    return (int)(1000.*multiplier);
+}
+/**
+ * 
+ */
+void simpler_INA219::autoZero()
+{
+    int old=ina219_currentScale;
+    for(int i=0;i<4;i++)
+    {
+        ina219_currentScale=i;
+        reconfigure();
+        int val=0;
+        for(int i=0;i<8;i++)
+        {
+            val+=getShuntVoltage_raw();
+            delay(1);
+        }
+        val=val/8;
+        ina219_zeros[i]=val;
+        
+    }
+    ina219_currentScale=old;    
 }
